d9d55f4dd469c5bc6b43ec2f9a3b2e66fb0b4aaf
caffix <caffix@users.noreply.github.com>
2021-09-20 15:25:39 -0400

    Fixed the method receiver for the memory methods

--- mem.go
+++ mem.go
@@ -5,22 +5,22 @@ import (
 	"strings"
 )
 
-func (s Set) memHas(element string) bool {
+func (s *Set) memHas(element string) bool {
 	_, exists := s.elements[strings.ToLower(element)]
 	return exists
 }
 
-func (s Set) memInsert(element string) {
+func (s *Set) memInsert(element string) {
 	s.elements[strings.ToLower(element)] = nothing{}
 }
 
-func (s Set) memInsertMany(elements ...string) {
+func (s *Set) memInsertMany(elements ...string) {
 	for _, i := range elements {
 		s.memInsert(i)
 	}
 }
 
-func (s Set) memRemove(element string) {
+func (s *Set) memRemove(element string) {
 	e := strings.ToLower(element)
 
 	delete(s.elements, e)
